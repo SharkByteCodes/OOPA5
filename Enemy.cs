13f29f6eaaef04e470c5c054ae9e539053c8dca1
SharkByteCodes <124321575+SharkByteCodes@users.noreply.github.com>
2025-06-20 16:25:43 -0400

    Add Enemy Sprites

--- Enemy.cs
+++ Enemy.cs
@@ -15,14 +15,27 @@ public class Enemy
     private Random _rng;
     
     
-    public Enemy() //not number 1. (non-arg constructor)
+    public Enemy() //not number 1. I realized this pun after naming the class still funny I think.
+                   //(non-arg constructor)
     {
         _rng = new Random();
         _position = new Vector2(_rng.Next(25,750),_rng.Next(25,750));  //set a random position
     }
 
-    //public Enemy(_position, _texture)
-    //{
-        
-    //}
+    public Enemy(Vector2 _position, Texture2D _texture) //arguemtned constructor
+    {
+        _texture = _texture;
+        _position = _position;
+    }
+
+    
+    // Some accessors
+    public Vector2 GetPosition() { return _position; }
+    
+    public void Draw(SpriteBatch spriteBatch)
+    {
+        spriteBatch.Begin();
+        spriteBatch.Draw(_texture, _position, Color.White);
+        spriteBatch.End();
+    }
 }
